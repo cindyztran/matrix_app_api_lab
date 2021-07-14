a1d7668ced197cc5196beadb1e98325440f68c5f
cindytran <cindyztran@gmail.com>
2021-07-14 16:26:08 -0700

    Commit 9: Reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "The One" },
+	{ name: "Trinity", description: "computer programmer and a hacker who has escaped from the Matrix" },
+	{ name: "Morpheus", description: "captain of the Nebuchadnezzar" },
+	{ name: "Agent Smith", description: "main antagonist" },
 ])
